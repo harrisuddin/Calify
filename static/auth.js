e31577b12cb9d5dcdd634fa4a440927949939f89
harrisuddin <harris7001@gmail.com>
2020-07-19 13:21:13 +0100

    added encrypt and decrypt functions

--- static/auth.js
+++ static/auth.js
@@ -1,19 +1,75 @@
-const jwt = require("jsonwebtoken");
+"use strict";
+// const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+const algorithm = "aes-256-cbc";
+const IV_LENGTH = 16; // For AES, this is always 16
+/**
+ * Note: 256 (in algorithm name) is key size.
+ * Block size for AES is always 128
+ */
+const KEY_LENGTH = 32;
 
-function verifyJWT(token) {
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    return decoded;
-  } catch (ex) {
-    //console.log(ex);
-    return null;
-  }
+/**
+ *
+ * @param {Buffer | String} password - The password to be used for generating key
+ * @param {Buffer | String} salt - The salt that is added to the key
+ * @return {Buffer} ```key```
+ * 
+ */
+const getKeyFromPassword = (password, salt) => {
+  return crypto.scryptSync(password, salt, KEY_LENGTH);
+};
+
+/**
+ * A helper function for getKeyFromPassword
+ * @param {Buffer | String} salt - The salt that is added to the key
+ */
+const getKey = (salt) => getKeyFromPassword(process.env.AUTH_SECRET, salt);
+
+/**
+ * Encrypt text with a salt.
+ * @param {Buffer | String} text - The clear text message to be encrypted
+ * @param {Buffer | String} salt - The salt to be used for encryption
+ */
+function encrypt(text, salt) {
+  let iv = crypto.randomBytes(IV_LENGTH);
+  let cipher = crypto.createCipheriv(algorithm, getKey(salt), iv);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return iv.toString("hex") + ":" + encrypted.toString("hex");
 }
 
-function generateJWT(_id, google_email) {
-  const token = jwt.sign({ _id, google_email }, process.env.JWT_SECRET);
-  return token;
+/**
+ * Decrypt text with a salt.
+ * @param {String} text - The ciphertext message to be decrypted
+ * @param {Buffer | String} salt - The salt to be used for decryption
+ */
+function decrypt(text, salt) {
+  let textParts = text.split(":");
+  let iv = Buffer.from(textParts.shift(), "hex");
+  let encryptedText = Buffer.from(textParts.join(":"), "hex");
+  let decipher = crypto.createDecipheriv(algorithm, getKey(salt), iv);
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
 }
 
-exports.verifyJWT = verifyJWT;
-exports.generateJWT = generateJWT; 
+// function verifyJWT(token) {
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     return decoded;
+//   } catch (ex) {
+//     //console.log(ex);
+//     return null;
+//   }
+// }
+
+// function generateJWT(_id, google_email) {
+//   const token = jwt.sign({ _id, google_email }, process.env.JWT_SECRET);
+//   return token;
+// }
+
+// exports.verifyJWT = verifyJWT;
+// exports.generateJWT = generateJWT;
+exports.encrypt = encrypt;
+exports.decrypt = decrypt;
--- static/helper.js
+++ static/helper.js
@@ -1,7 +1,7 @@
 function encodeFormData(data) {
   return Object.keys(data)
-  .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-  .join("&");
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
 }
 
 function getURLWithParams(url, params) {
@@ -11,7 +11,7 @@ function getURLWithParams(url, params) {
 // t is in seconds
 // returns the time it will be in t seconds as a string
 function getExpiryTime(t) {
-  return new Date(Date.now() + (t * 1000)).toString();
+  return new Date(Date.now() + t * 1000).toString();
 }
 
 function sleep(ms) {
@@ -20,17 +20,23 @@ function sleep(ms) {
   });
 }
 
-/** 
+/**
  * returns true if the time difference is more than or equal to the maxDifference
  * maxDifference should be in seconds
-*/
+ */
 function compareTimeDifference(t1, t2, maxDifference) {
   var date1 = new Date(t1);
   var date2 = new Date(t2);
 
   var diff = Math.floor((date2 - date1) / 1000); // difference in seconds
 
-  return (diff >= maxDifference);
+  return diff >= maxDifference;
 }
 
-module.exports = {encodeFormData, getURLWithParams, compareTimeDifference, getExpiryTime, sleep};
+module.exports = {
+  encodeFormData,
+  getURLWithParams,
+  compareTimeDifference,
+  getExpiryTime,
+  sleep
+};
